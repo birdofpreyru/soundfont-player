6d80bbeec10a5101f5e2d8777a56632778b3fefa
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-10-25 23:07:29 +0100

    Migrated to @dr.pogodin/audio-loader

--- test/soundfont-player-test.js
+++ test/soundfont-player-test.js
@@ -3,10 +3,12 @@ require('web-audio-test-api')
 var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
-var load = require('audio-loader')
+var load = require('@dr.pogodin/audio-loader')
 var Soundfont = require('..')
 
 var piano = fs.readFileSync(path.join(__dirname, '../examples/assets/acoustic_grand_piano-ogg.js'))
+
+const SF_BASE = 'https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages';
 
 load.fetch = function (url) {
   load.fetch.url = url
@@ -17,44 +19,48 @@ describe('Soundfont player', function () {
   describe('Load instruments', function () {
     it('returns a promise', function () {
       var ac = new AudioContext()
-      assert.equal(typeof Soundfont.instrument(ac, 'piano').then, 'function')
+      assert.strictEqual(typeof Soundfont.instrument(ac, 'piano')
+        .then, 'function')
     })
-    it('loads mp3 by default', function () {
+    it('loads mp3 by default', () => {
       var ac = new AudioContext()
-      return Soundfont.instrument(ac, 'piano').then(function (piano) {
-        assert.equal(piano.url,
-          'https://gleitz.github.io/midi-js-soundfonts/MusyngKite/piano-mp3.js')
-      })
+      return Soundfont.instrument(ac, 'acoustic_grand_piano')
+        .then(function (piano) {
+          assert.strictEqual(
+            piano.url,
+            `${SF_BASE}/MusyngKite/acoustic_grand_piano-mp3.js`,
+          )
+        })
     })
-    it('the promise resolve to an instrument', function () {
+    it('the promise resolve to an instrument', () => {
       var ac = new AudioContext()
-      return Soundfont.instrument(ac, 'piano')
-      .then(function (piano) {
+      return Soundfont.instrument(ac, 'acoustic_grand_piano')
+      .then((piano) => {
         assert(piano)
-        assert.equal(piano.name, 'piano')
-        assert.equal(typeof piano.play, 'function')
+        assert.strictEqual(piano.name, 'acoustic_grand_piano')
+        assert.strictEqual(typeof piano.play, 'function')
       })
     })
     it('options.nameToUrl', function () {
+      const TEST_URL = `${SF_BASE}/MusyngKite/acoustic_grand_piano-mp3.js`;
       var ac = new AudioContext()
-      var toUrl = function (name) { return 'URL:' + name + '.js' }
-      return Soundfont.instrument(ac, 'piano', { nameToUrl: toUrl }).then(function (piano) {
-        assert.equal(piano.url, 'URL:piano.js')
-      })
+      var toUrl = () => TEST_URL;
+      return Soundfont.instrument(ac, 'xxx', { nameToUrl: toUrl })
+        .then((piano) => assert.strictEqual(piano.url, TEST_URL));
     })
   })
   describe('Build urls', function () {
     it('get default url', function () {
-      assert.equal(Soundfont.nameToUrl('marimba'),
-        'https://gleitz.github.io/midi-js-soundfonts/MusyngKite/marimba-mp3.js')
+      assert.strictEqual(Soundfont.nameToUrl('marimba'),
+        `${SF_BASE}/MusyngKite/marimba-mp3.js`);
     })
-    it('get MusyngKite url', function () {
-      assert.equal(Soundfont.nameToUrl('marimba', 'FluidR3_GM'),
-        'https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/marimba-mp3.js')
+    it('get FluidR3_GM url', function () {
+      assert.strictEqual(Soundfont.nameToUrl('marimba', 'FluidR3_GM'),
+        `${SF_BASE}/FluidR3_GM/marimba-mp3.js`)
     })
     it('accepts ogg', function () {
-      assert.equal(Soundfont.nameToUrl('marimba', null, 'ogg'),
-        'https://gleitz.github.io/midi-js-soundfonts/MusyngKite/marimba-ogg.js')
+      assert.strictEqual(Soundfont.nameToUrl('marimba', null, 'ogg'),
+        `${SF_BASE}/MusyngKite/marimba-ogg.js`)
     })
   })
 })
